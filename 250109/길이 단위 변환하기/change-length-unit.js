c25b287a9b41c2fc2f76f02b2b54c08c7c559e73
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-09 11:36:44 +0900

    Title: 길이 단위 변환하기, Time: 158ms, Memory: 14MB, Status: Passed - CodeTree

--- 250109/길이 단위 변환하기/change-length-unit.js
+++ 250109/길이 단위 변환하기/change-length-unit.js
@@ -4,5 +4,5 @@ let mi = 160934
 var a = 9.2 * ft
 var b = 1.3 * mi
 
-console.log(`9.2ft = ${a.toFixed(1)}`)
-console.log(`1.3mi = ${b.toFixed(1)}`)
+console.log(`9.2ft = ${a.toFixed(1)}cm`)
+console.log(`1.3mi = ${b.toFixed(1)}cm`)
