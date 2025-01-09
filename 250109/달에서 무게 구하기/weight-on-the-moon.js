83565d0334d081d3a907171bec85efe818190f93
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-09 11:24:05 +0900

    Title: 달에서 무게 구하기, Time: 169ms, Memory: 18MB, Status: Passed - CodeTree

--- 250109/달에서 무게 구하기/weight-on-the-moon.js
+++ 250109/달에서 무게 구하기/weight-on-the-moon.js
@@ -1,5 +1,11 @@
-var a = 13
-var b = 0.165000
-var c = (a * b).toFixed(6)
+// var a = 13
+// var b = 0.165000
+// var c = (a * b).toFixed(6)
 
-console.log(a + " * " + b.toFixed(6) + " = " + c)
+// console.log(a + " * " + b.toFixed(6) + " = " + c)
+
+let weight = 13;
+let ratio = 0.165;
+let result = weight * ratio;
+
+console.log(`${weight} * ${ratio.toFixed(6)} = ${result.toFixed(6)}`)
