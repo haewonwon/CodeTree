68f63e73c5e3af0b6137ab07c760492df07c2131
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-11 20:48:49 +0900

    Title: 실수 입력받아 계산, Time: 170ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250111/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
+++ 250111/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 
-let input = fs.readFileSync(0).toString().split("\n");
+let input = Number(fs.readFileSync(0).toString().split("\n"));
 
 let a = input[0];
 let b = input[1];
-let sum = a + b
+let sum = a + b;
 
-console.log(sum.toFixed(2))
+console.log(sum.toFixed(2));
