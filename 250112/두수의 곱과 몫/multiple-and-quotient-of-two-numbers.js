9c7a76cd2c6cad46adbfc480b94e13b7687f6e8c
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-12 17:34:31 +0900

    Title: 두수의 곱과 몫, Time: 159ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250112/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim();
+
+let arr = input.split(" ");
+
+let a = Number(arr[0]);
+let b = Number(arr[1]);
+
+console.log(`${a} * ${b} = ${a*b}`);
+console.log(`${a} / ${b} = ${parseInt(a/b)}`);
