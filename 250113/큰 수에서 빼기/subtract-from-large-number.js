cb2c6bf14612cba8b6f6764a2e7f61ce9faf3e58
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-13 17:35:16 +0900

    Title: 큰 수에서 빼기, Time: 170ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250113/큰 수에서 빼기/subtract-from-large-number.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim();
+let arr = input.split(" ");
+
+let a = Number(arr[0]);
+let b = Number(arr[1]);
+
+if (a > b) {
+    console.log(a - b)
+} else {
+    console.log(b - a)
+};
