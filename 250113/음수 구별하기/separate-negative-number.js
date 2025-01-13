e44b84fe528baf0cd32189f0412566c525f82f0c
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-13 17:23:26 +0900

    Title: 음수 구별하기, Time: 172ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250113/음수 구별하기/separate-negative-number.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim();
+
+if (input < 0) {
+    console.log(input)
+    console.log("minus");
+} else {
+    console.log(input)
+};
