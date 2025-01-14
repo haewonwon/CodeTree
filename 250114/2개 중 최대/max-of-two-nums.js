04dc0662fc17342eea7d35ee4344cb0212cb87cd
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-14 23:42:53 +0900

    Title: 2개 중 최대, Time: 173ms, Memory: 15MB, Status: Passed - CodeTree

--- 250114/2개 중 최대/max-of-two-nums.js
+++ 250114/2개 중 최대/max-of-two-nums.js
@@ -1,14 +1,17 @@
 const fs = require("fs");
 
 let input = fs.readFileSync(0).toString().trim();
-
 let arr = input.split(" ");
 
 let a = Number(arr[0]);
 let b = Number(arr[1]);
 
-if (a > b) {
-    console.log(a);
-} else {
-    console.log(b);
-};
+// if (a > b) {
+//     console.log(a);
+// } else {
+//     console.log(b);
+// };
+
+let result = a > b ? a : b;
+
+console.log(result);
