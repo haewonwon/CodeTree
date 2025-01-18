874bf3aade27952957e9529d3feceb69952dffcf
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-18 19:37:42 +0900

    Title: 4번의 크기 비교, Time: 196ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250118/4번의 크기 비교/4-time-comparison.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim();
+
+let arr = input.split("\n");
+let arr2 = arr[1].split(" ")
+
+let a = Number(arr[0]);
+let b = Number(arr2[0]);
+let c = Number(arr2[1]);
+let d = Number(arr[2]);
+let e = Number(arr[3]);
+
+if (a > b) {
+    console.log("1")
+} else {
+    console.log("0")
+}
+
+if (a > c) {
+    console.log("1")
+} else {
+    console.log("0")
+}
+
+if (a > d) {
+    console.log("1")
+} else {
+    console.log("0")
+}
+
+if (a > e) {
+    console.log("1")
+} else {
+    console.log("0")
+}
