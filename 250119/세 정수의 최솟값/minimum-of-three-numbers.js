db1ec4588d8aa2d96d893ebd51f36bd46b13335f
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-19 18:36:05 +0900

    Title: 세 정수의 최솟값, Time: 183ms, Memory: 15MB, Status: Runtime Error - CodeTree

--- 250119/세 정수의 최솟값/minimum-of-three-numbers.js
+++ 250119/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -9,13 +9,16 @@ let c = Number(arr[2]);
 
 let min;
 
-if (b >= a && c >= a) {
-    min = a
-} else if (a >= b && c >= b) {
-    min = b
-} else {
-    min = c
-}
+let min = (a <= b && a <= c) ? a : (b <= a && b <= c) ? b : c;
+
+// if (b >= a && c >= a) {
+//     min = a
+// } else if (a >= b && c >= b) {
+//     min = b
+// } else {
+//     min = c
+// }
+
 // a >= b ? min = b : min = a;
 // a >= c ? min = c : min = a;
 // b >= c ? min = c : min = b;
