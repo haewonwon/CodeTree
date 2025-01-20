e284e743e56d25bc4fc56d1a331c5fcb87457f6e
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-20 17:52:12 +0900

    Title: 수의 계절은, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

--- 250120/수의 계절은/season-of-num.js
+++ 250120/수의 계절은/season-of-num.js
@@ -2,12 +2,24 @@ const fs = require("fs");
 
 let m = Number(fs.readFileSync(0).toString().trim());
 
-if (m == 3 || m == 4 || m == 5) {
-    console.log("Spring")
-} else if (m == 6 || m == 7 || m == 8) {
-    console.log("Summer")
-} else if (m == 9 || m == 10 || m == 11) {
-    console.log("Fall")
-} else if (m == 12 || m == 1 || m == 2) {
+// 내가 작성한 답
+// if (m == 3 || m == 4 || m == 5) {
+//     console.log("Spring")
+// } else if (m == 6 || m == 7 || m == 8) {
+//     console.log("Summer")
+// } else if (m == 9 || m == 10 || m == 11) {
+//     console.log("Fall")
+// } else if (m == 12 || m == 1 || m == 2) {
+//     console.log("Winter")
+// }
+
+// 해설
+if (m >= 12 || m <= 2) {
     console.log("Winter")
+} else if (m <= 5) {
+    console.log("Spring")
+} else if (m <= 8) {
+    console.log("Summer")
+} else {
+    console.log("Fall")
 }
