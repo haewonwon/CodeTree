6072f22536fbe7edfad5f429967e931712f44526
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-20 18:01:04 +0900

    Title: 장학금, Time: 190ms, Memory: 15MB, Status: Passed - CodeTree

--- 250120/장학금/scholarship.js
+++ 250120/장학금/scholarship.js
@@ -6,10 +6,18 @@ let arr = input.split(" ");
 let a = Number(arr[0]);
 let b = Number(arr[1]);
 
-if (a >= 90 && b >= 95) {
-    console.log("100000")
-} else if (a >= 90 && b >= 90) {
-    console.log("50000")
+// 내가 작성한 코드 & 해설
+// if (a >= 90 && b >= 95) {
+//     console.log("100000")
+// } else if (a >= 90 && b >= 90) {
+//     console.log("50000")
+// } else {
+//     console.log("0")
+// }
+
+// 효율적인 코드
+if (a >= 90) {
+    console.log(b >= 95 ? "100000" : b >= 90 ? "50000" : "0");
 } else {
-    console.log("0")
+    console.log("0");
 }
