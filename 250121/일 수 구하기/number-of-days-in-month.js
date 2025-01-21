44d5262764109c54534c9f1e1c8f04fb1dca307d
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-21 17:07:05 +0900

    Title: 일 수 구하기, Time: 173ms, Memory: 15MB, Status: Passed - CodeTree

--- 250121/일 수 구하기/number-of-days-in-month.js
+++ 250121/일 수 구하기/number-of-days-in-month.js
@@ -2,10 +2,32 @@ const fs = require("fs");
 
 let n = Number(fs.readFileSync(0).toString().trim());
 
-if (n == 2) {
-    console.log("28")
-} else if ((n <= 6 && n % 2 !== 0) || n == 7 || n == 8 || (n >= 9 && n % 2 == 0)) {
-    console.log("31")
-} else {
-    console.log("30")
+// 내가 작성한 답
+// if (n == 2) {
+//     console.log("28")
+// } else if ((n <= 6 && n % 2 !== 0) || n == 7 || n == 8 || (n >= 9 && n % 2 == 0)) {
+//     console.log("31")
+// } else {
+//     console.log("30")
+// }
+
+// 해설
+if (n === 2) {
+    console.log(28);
+}
+else if (n <= 7) {
+    if (n % 2 === 1) {
+        console.log(31);
+    }
+    else {
+        console.log(30);
+    }
+}
+else {
+    if (n % 2 === 0) {
+        console.log(31);
+    }
+    else {
+        console.log(30);
+    }
 }
