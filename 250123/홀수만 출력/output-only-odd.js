bb7ca81b4f6d5c5d12e1553663d0a83106e87144
해원 <139232797+haewonwon@users.noreply.github.com>
2025-01-23 19:31:36 +0900

    Title: 홀수만 출력, Time: 167ms, Memory: 15MB, Status: Passed - CodeTree

--- 250123/홀수만 출력/output-only-odd.js
+++ 250123/홀수만 출력/output-only-odd.js
@@ -5,18 +5,29 @@ let arr = input.split(" ");
 let a = Number(arr[0]);
 let b = Number(arr[1]);
 
+// 내가 작성한 코드
+// let result = "";
+
+// if (a % 2 == 0) {
+//     for (let i = a+1; i <= b; i += 2) {
+//         result += (i + " ")
+//     }
+
+//     console.log(result)
+// } else {
+//     for (let i = a; i <= b; i += 2) {
+//         result += (i + " ")
+//     }
+
+//     console.log(result)
+// }
+
+// 챗지피티가 리팩토링해 준 코드
+let start = (a % 2 === 0) ? a + 1 : a;
 let result = "";
 
-if (a % 2 == 0) {
-    for (let i = a+1; i <= b; i += 2) {
-        result += (i + " ")
-    }
-
-    console.log(result)
-} else {
-    for (let i = a; i <= b; i += 2) {
-        result += (i + " ")
-    }
-
-    console.log(result)
+for (let i = start; i <= b; i += 2) {
+    result += (i + " ");
 }
+
+console.log(result);
