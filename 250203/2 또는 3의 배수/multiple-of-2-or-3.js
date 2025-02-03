fe160f65ad27053326f10649d85e547c7ead53ee
해원 <139232797+haewonwon@users.noreply.github.com>
2025-02-03 23:26:36 +0900

    Title: 2 또는 3의 배수, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250203/2 또는 3의 배수/multiple-of-2-or-3.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let arr =fs.readFileSync(0).toString().trim().split(" ");
+let n = Number(arr[0]);
+
+let result = "";
+
+for (let i = 1; i <= n; i ++) {
+    if (i % 2 == 0 || i % 3 == 0) {
+        result += (1 + " ")
+    } else {
+        result += (0 + " ")
+    }
+}
+
+console.log(result)
