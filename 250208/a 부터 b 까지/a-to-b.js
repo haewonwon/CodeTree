f21c24765a80cb7b9c1f5bb95cf9690b2b55777b
해원 <139232797+haewonwon@users.noreply.github.com>
2025-02-08 22:45:57 +0900

    Title: a 부터 b 까지, Time: 174ms, Memory: 15MB, Status: Passed - CodeTree

--- 250208/a 부터 b 까지/a-to-b.js
+++ 250208/a 부터 b 까지/a-to-b.js
@@ -6,15 +6,15 @@ let b = Number(arr[1]);
 let result = "";
 
 for (let i = a; i <= b; ) {
-    if (a < b) {
-        result += (i + " ")
+    result += (i + " ")
 
-        if (i % 2 === 1) {
-            i *= 2
-        } else {
-            i += 3
-        }
+    if (i % 2 === 1) {
+        i *= 2
+    } else {
+        i += 3
     }
+
+    if (i > b) break;  
 }
 
 console.log(result)
