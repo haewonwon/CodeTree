bc86a4fbc4bbcd46a148a208534db444bab585a1
해원 <139232797+haewonwon@users.noreply.github.com>
2025-02-08 22:39:32 +0900

    Title: a 부터 b 까지, Time: 5028ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

---
+++ 250208/a 부터 b 까지/a-to-b.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+let arr = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(arr[0]);
+let b = Number(arr[1]);
+
+let result = "";
+
+for (let i = a; i <= b;) {
+    if (a < b) {
+        result += (i + " ")
+
+        if (i % 2 == 1) {
+            i *= 2
+        } else {
+            i += 3
+        }
+    }
+}
+
+console.log(result)
