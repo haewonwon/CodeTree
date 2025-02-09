43769df36e2ea39f33f1fca8c05fa53fa109948d
해원 <139232797+haewonwon@users.noreply.github.com>
2025-02-09 23:30:17 +0900

    Title: 369 게임, Time: 169ms, Memory: 15MB, Status: Passed - CodeTree

--- 250209/369 게임/369-game.js
+++ 250209/369 게임/369-game.js
@@ -8,6 +8,8 @@ for (let i = 1; i <= num; i ++) {
 
     if (i % 3 === 0 || str.includes('3') || str.includes('6') || str.includes('9')) {
         result += (0 + " ")
+
+        // i % 10 === 3 || i % 10 === 6 || i % 10 === 9
     } else {
         result += (i + " ")
     }
