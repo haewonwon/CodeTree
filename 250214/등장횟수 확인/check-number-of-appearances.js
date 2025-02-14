63ea44b071c63fe725ec3d6664e92e9a32bf947f
해원 <139232797+haewonwon@users.noreply.github.com>
2025-02-14 22:35:28 +0900

    Title: 등장횟수 확인, Time: 193ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250214/등장횟수 확인/check-number-of-appearances.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let arr = fs.readFileSync(0).toString().trim().split("\n").map(Number);
+
+let cnt = 0;
+
+for (let i = 0; i < arr.length; i ++) {
+    if (arr[i] % 2 === 0) {
+        cnt ++;
+    }
+}
+
+console.log(cnt)
