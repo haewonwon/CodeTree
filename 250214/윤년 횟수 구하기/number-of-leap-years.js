da15227b7468ad37e356a8fa5627b54496859d89
해원 <139232797+haewonwon@users.noreply.github.com>
2025-02-14 22:27:29 +0900

    Title: 윤년 횟수 구하기, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250214/윤년 횟수 구하기/number-of-leap-years.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let cnt = 0;
+
+for (let i = 1; i <= n; i++) {
+    if (i % 4 === 0) {
+        cnt ++;
+    }
+    
+    if (i % 100 === 0 && i % 400 !== 0) {
+        cnt --;
+    }
+}
+
+console.log(cnt)
