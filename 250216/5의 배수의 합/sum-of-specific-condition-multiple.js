2c4876beb4b499bfe2131ee232d38481191c20bd
해원 <139232797+haewonwon@users.noreply.github.com>
2025-02-16 19:11:30 +0900

    Title: 5의 배수의 합, Time: 186ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250216/5의 배수의 합/sum-of-specific-condition-multiple.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+let arr = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+let a = arr[0];
+let b = arr[1];
+
+let sumVal = 0;
+
+if (b >= a) {
+    for (let i = a; i <= b; i++) {
+        if (i % 5 === 0) {
+            sumVal += i;
+        }
+    }
+} else {
+    for (let i = b; i <= a; i ++) {
+        if (i % 5 === 0) {
+            sumVal += i;
+        }
+    }
+}
+
+console.log(sumVal);
