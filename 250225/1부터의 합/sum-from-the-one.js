f97ccb13429e8985cd693f9f43b972902a5a8b0f
해원 <139232797+haewonwon@users.noreply.github.com>
2025-02-25 06:06:06 +0900

    Title: 1부터의 합, Time: 191ms, Memory: 15MB, Status: Passed - CodeTree

--- 250225/1부터의 합/sum-from-the-one.js
+++ 250225/1부터의 합/sum-from-the-one.js
@@ -2,15 +2,16 @@ const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 
 let sumVal = 0;
+let ans;
 
 for (let i = 1; i <= 100; i++) {
     sumVal += i;
 
     if (sumVal >= n) {
-        sumVal -= i
+        ans = i;
         break;
     }
 }
 
-console.log(sumVal)
+console.log(ans);
 
