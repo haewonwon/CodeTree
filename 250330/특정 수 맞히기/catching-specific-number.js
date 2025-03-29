a7426506e1cee2319f2fb76ab8ac3cc2055262a0
해원 <139232797+haewonwon@users.noreply.github.com>
2025-03-30 00:02:50 +0900

    Title: 특정 수 맞히기, Time: 179ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250330/특정 수 맞히기/catching-specific-number.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let i = 0;
+
+while (true) {
+    let n = input[i]
+    i++
+
+    if (n < 25) {
+        console.log("Higher");
+    } else if (n > 25) {
+        console.log("Lower");
+    } else {
+        console.log("Good")
+        break
+    }
+}
