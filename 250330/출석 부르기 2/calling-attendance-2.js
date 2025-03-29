dcca3cc1b6d9e1073654037ef3c68f4c9123a275
해원 <139232797+haewonwon@users.noreply.github.com>
2025-03-30 00:07:46 +0900

    Title: 출석 부르기 2, Time: 159ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250330/출석 부르기 2/calling-attendance-2.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let i = 0;
+
+while (true) {
+    let n = Number(input[i]);
+    i++
+
+    if (n === 1) {
+        console.log("John");
+    } else if (n === 2) {
+        console.log("Tom");
+    } else if (n === 3) {
+        console.log("Paul");
+    } else if (n === 4) {
+        console.log("Sam");
+    } else {
+        console.log("Vacancy");
+        break;
+    }
+}
