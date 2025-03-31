5dec9ba340688ad9309c7be584cc1f94af0748e7
해원 <khcw1029@daum.net>
2025-03-31 23:58:47 +0900

    Title: 3N + 1 수열, Time: 5061ms, Memory: 15MB, Status: Time Limit Exceed - Codetree

--- 250331/3N + 1 수열/3n-plus-1-sequence.js
+++ 250331/3N + 1 수열/3n-plus-1-sequence.js
@@ -4,17 +4,17 @@ let n = Number(fs.readFileSync(0).toString());
 let count = 0;
 
 while (true) {
-    count ++;
-
     if (n === 1) {
         break;
     }
 
     if (n % 2 === 0) {
+        count ++;
         n = n * 2
     } else {
+        count ++;
         n = n * 3 + 1
     }
-
-    console.log(count);
 }
+
+console.log(count);
