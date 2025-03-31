7da80ab43600dd7fe68433c4f1d1626cc8b35958
해원 <139232797+haewonwon@users.noreply.github.com>
2025-03-31 23:07:42 +0900

    Title: 20대의 평균 나이, Time: 5082ms, Memory: 17MB, Status: Time Limit Exceed - Codetree

---
+++ 250331/20대의 평균 나이/average-age-of-20.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let i = 0;
+let sum = 0;
+
+while (true) {
+    let n = Number(input[i]);
+
+    if (n < 30) {
+        sum += n;
+        i++;
+    }
+
+    if (n >= 30) {
+        break;
+    }
+}
+
+console.log((sum / i).toFixed(2));
