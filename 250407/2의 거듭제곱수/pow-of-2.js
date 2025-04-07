86f500caac7e675c2e6e187a3533f6a1e95c28f8
해원 <khcw1029@daum.net>
2025-04-07 23:38:37 +0900

    Title: 2의 거듭제곱수, Time: 180ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250407/2의 거듭제곱수/pow-of-2.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let i = 0;
+
+while (true) {
+    if (n > 2**i) {
+        i ++;
+    } else if (n == 2**i) {
+        console.log(i)
+        break;
+    }
+}
