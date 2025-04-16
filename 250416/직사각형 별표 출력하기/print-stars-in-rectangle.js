3c50e65561bf6a5fa919d20560450c566f4c8524
해원 <khcw1029@daum.net>
2025-04-16 23:06:56 +0900

    Title: 직사각형 별표 출력하기, Time: 172ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250416/직사각형 별표 출력하기/print-stars-in-rectangle.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+let arr = fs.readFileSync(0).toString().trim().split(" ");
+
+let n = Number(arr[0]);
+let m = Number(arr[1]);
+
+let str = "";
+
+for (let i = 0; i < n; i++) {
+    str = "";
+
+    for (let i = 0; i < m; i ++) {
+        str += "*" + " ";
+    }
+
+    console.log(str);
+}
