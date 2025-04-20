02ad94fd9fd9880134d3b369f31cd7cdf635f1eb
해원 <khcw1029@daum.net>
2025-04-20 16:39:50 +0900

    Title: 배열 채우기, Time: 190ms, Memory: 15MB, Status: Passed - Codetree

--- 250420/배열 채우기/filling-array.js
+++ 250420/배열 채우기/filling-array.js
@@ -1,24 +1,38 @@
+// 내가 작성한 코드
+// const fs = require("fs");
+// let arr = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+// let newArr = Array(10);
+
+// let n = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== 0) {
+//         n = i;
+//         newArr.push(arr[i]);
+//     } else {
+//         break;
+//     }
+// }
+
+// let result = "";
+// newArr = newArr.slice(10)
+
+// for (let j = n; j >= 0; j--) {
+//     result += newArr[j] + " ";
+// }
+
+// console.log(result);
+
+// 챗지피티가 리팩토링한 코드
 const fs = require("fs");
-let arr = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+const arr = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-let newArr = Array(10);
+const resultArr = [];
 
-let n = 0;
-
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 0) {
-        n = i;
-        newArr.push(arr[i]);
-    } else {
-        break;
-    }
+for (const num of arr) {
+    if (num === 0) break;
+    resultArr.push(num);
 }
 
-let result = "";
-newArr = newArr.slice(10)
-
-for (let j = n; j >= 0; j--) {
-    result += newArr[j] + " ";
-}
-
-console.log(result);
+console.log(resultArr.reverse().join(" "));
